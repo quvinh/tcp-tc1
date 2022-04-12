edf975c6e887d791705d4c5e73bed9e1d3259ccd
quvinh <vinhhp2620@gmail.com>
2022-04-12 16:14:56 +0700

    finish TC1

--- client/client.js
+++ client/client.js
@@ -1,7 +1,6 @@
 const net = require('net');
 const readline = require('readline-sync')
 const moment = require('moment')
-var time = moment().format('ss')
 
 const options = {
     port: 4000,
@@ -9,19 +8,20 @@ const options = {
 }
 
 const client = net.createConnection(options)
-let timeOut = 30
+let timeOut = 5
 client.on('connect', ()=>{
     client.write('Da ket noi')
     console.log('_______Client_______\n')
     console.log('Xin chao!\n')
     let tmrID = setInterval(() => {
-        process.stdout.write(`\r=>Ket noi se dong(${timeOut}s)`)
+        process.stdout.write(`\r=>Ket noi se dong (trong ${timeOut}s)`)
         if(timeOut>0) 
         {
             timeOut--
         } else {
             client.end()
-            console.log(`\nDa ngat ket noi voi server`)
+            console.clear()
+            console.log(`\n=> Da ngat ket noi voi server`)
             clearInterval(tmrID)
         }
     }, 1000)
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var array = []
 
 server.on('connection', (socket)=>{
     socket.on('data', (data)=>{
-        console.log('\n*****\n=>Client IP:' + socket.remoteAddress.substring(7) + ' PORT:' + socket.remotePort + '\n=>Data: ' + data + '\n*****')
+        console.log('\n*****\n=>Client IP:' + socket.remoteAddress.substring(7) + ' - PORT:' + socket.remotePort + '\n=>Data: ' + data + '\n*****')
         // socket.write('=> Them du lieu tu IP:' + socket.remoteAddress + ' PORT: ' + socket.remotePort)
         countConn()
         console.log(`__________\n|\n|\tKet noi/Ket noi cung IP:\n|\t${array.length} / ${array.map(ip => ip.ip === socket.remoteAddress.substring(7)).length}\n|__________`)
@@ -35,7 +35,7 @@ server.on('connection', (socket)=>{
 
 server.listen(4000, ()=>{
     // console.log('==>Server dang hoat dong o PORT:', server.address().port)
-    console.log('_______Server_______')
+    console.log('_TC1-Ngo Quang Vinh-80413\n_______Server_______')
     let timeLog = ''
     process.stdout.write(`Loading ...`)
 
